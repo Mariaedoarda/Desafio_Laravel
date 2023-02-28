6c0cb382e15e4d5b654114641316fdd4eca37514
Mariaedoarda <maria.moreira115@etec.sp.gov.br>
2023-02-28 11:04:49 -0300

    first commit

---
+++ resources/views/empresa.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <a href="/home">Home</a>
+    <a href="/produtos">Produtos</a>
+    <a href="/contato">Contato</a>
+    <a href="/empresa">Empresa</a>
+
+    <h1>EMPRESA MARIA EDUARDA</h1>
+</html>
